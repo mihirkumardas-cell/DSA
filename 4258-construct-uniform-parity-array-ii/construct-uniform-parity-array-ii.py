c14c2c581c68a103697472cc14577128004d5266
Mihir-07 <mihirkkumardas@gmail.com>
2026-09-03 21:38:53 +0530

    Time: 19 ms (81.10%) | Memory: 35.3 MB (51.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,19 +1,12 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        min_o=min_e=None
-        has_o=has_e=False
+        minimum=min(nums1)
+        if minimum%2==1:
+            return True
         for i in nums1:
             if i%2==1:
-                has_o=True
-                if min_o is None or i<min_o:
-                    min_o=i
-            else:
-                has_e=True
-                if min_e is None or i<min_e:
-                    min_e=i
-        all_e=not has_o
-        all_o=not has_e or (has_o and min_o < min_e)
-        return all_o or all_e
+                return False
+        return True
             
             
         
